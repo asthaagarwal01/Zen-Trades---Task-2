2f6c38f260c01e2596c2a4296a7dcc5d00c49dc8
asthaagarwal01 <astha2agarwal5@gmail.com>
2023-12-30 17:10:05 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,{ useState, useEffect } from 'react';
+import axios from 'axios';
+import ImportProducts from './ImportProducts';
+import ProductsTable from './ProductsTable';
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+  const [displayFields, setDisplayFields] = useState(['Title', 'Price', 'Popularity']);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await axios.get('https://s3.amazonaws.com/open-to-cors/assignment.json');
+      const sortedData = Object.values(response.data.products).sort((a, b) => b.popularity - a.popularity);
+      setProducts(sortedData);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Import Products</h1>
+      <ImportProducts displayFields={displayFields} setDisplayFields={setDisplayFields} />
+      <ProductsTable products={products} displayFields={displayFields} />
     </div>
   );
 }
